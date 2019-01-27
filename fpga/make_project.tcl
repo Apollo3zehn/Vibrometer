ed9f68d8594fcd437aee921230d5924f1e215585
Apollo3zehn <vwilms@outlook.com>
2019-01-27 22:30:49 +0100

    FIx minor issues.

--- fpga/make_project.tcl
+++ fpga/make_project.tcl
@@ -1,19 +1,14 @@
 variable location [file dirname [file normalize [info script]]]
 
-if { $argc != 1 } {
-    puts "The make_project script requires a project name."
-    puts "Please try again."
-} else {
-    set project_name [lindex $argv 0]
-    set origin_dir_loc $location/$project_name
+set project_name [lindex $argv 0]
+set origin_dir_loc $location/$project_name
 
-    file delete -force ./../artifacts/fpga/$project_name
-    file mkdir ./../artifacts/fpga
+file delete -force ./../artifacts/fpga/$project_name
+file mkdir ./../artifacts/fpga
 
-    cd ./../artifacts/fpga
+cd ./../artifacts/fpga
 
-    source $location/$project_name/block_design.tcl
+source $location/$project_name/block_design.tcl
 
-    make_wrapper -files [get_files $location/$project_name/system.bd] -top
-    add_files -norecurse $location/$project_name/system_wrapper.v
-}
+make_wrapper -files [get_files $location/$project_name/system.bd] -top
+add_files -norecurse $location/$project_name/system_wrapper.v
