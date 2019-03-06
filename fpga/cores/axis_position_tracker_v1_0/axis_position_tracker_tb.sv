59097548f06419011a8ec8efef90fdbf6b2af7db
Apollo3zehn <vwilms@outlook.com>
2019-03-06 14:33:41 +0100

    Update test bench files.

--- fpga/cores/axis_position_tracker_v1_0/axis_position_tracker_tb.sv
+++ fpga/cores/axis_position_tracker_v1_0/axis_position_tracker_tb.sv
@@ -2,68 +2,95 @@
 
 module axis_position_tracker_tb #
 (
-    parameter integer             AXIS_TDATA_WIDTH    = 32
+    parameter integer                   S_AXIS_TDATA_WIDTH  = 32
 );
 
-    logic                         aclk                = 0;
-    logic                         aresetn             = 0;
-    logic [AXIS_TDATA_WIDTH-1:0]  lower_treshold      = -10;
-    logic [AXIS_TDATA_WIDTH-1:0]  upper_treshold      = 10;
-    logic                         S_AXIS_tvalid       = 1;
-    logic [AXIS_TDATA_WIDTH-1:0]  S_AXIS_tdata        = 0;
+    logic                               aclk                = 0;
+    logic                               aresetn             = 0;
+    logic [(S_AXIS_TDATA_WIDTH/2)-1:0]  lower_threshold     = -1;
+    logic [(S_AXIS_TDATA_WIDTH/2)-1:0]  upper_threshold     = 1;
+    logic [4:0]                         log_scale           = 0;
+    logic                               S_AXIS_tvalid       = 1;
+    logic [S_AXIS_TDATA_WIDTH-1:0]      S_AXIS_tdata;
 
-    integer i                                       = 0;
+    integer i                                               = 0;
+
+    logic [(S_AXIS_TDATA_WIDTH/2)-1:0]  signal_a            = 0;
+    logic [(S_AXIS_TDATA_WIDTH/2)-1:0]  signal_b            = 15;
+
+    assign S_AXIS_tdata                 = {signal_b, signal_a};
 
     axis_position_tracker DUT (
         .aclk(aclk),
         .aresetn(aresetn),
-        .lower_treshold(lower_treshold),
-        .upper_treshold(upper_treshold),
+        .lower_threshold(lower_threshold),
+        .upper_threshold(upper_threshold),
+        .log_scale(log_scale),
         .S_AXIS_tvalid(S_AXIS_tvalid),
         .S_AXIS_tdata(S_AXIS_tdata)
     );
      
     initial begin
 
-        repeat (1) @(posedge aclk);                     
-            S_AXIS_tdata        = -5;
-            aresetn             = 1;
+        repeat (2) @(posedge aclk);                     
+            aresetn             = 1'b1;
         
-        repeat (1) @(posedge aclk); 
-            for (i=0; i<6; i=i+1) begin
+        repeat (5) @(posedge aclk); 
+            for (i=0; i<10; i=i+1) begin
                 repeat (1) @(posedge aclk); 
-                    S_AXIS_tdata = 10; 
+                    signal_a = 15; 
                 repeat (1) @(posedge aclk); 
-                    S_AXIS_tdata = 5;
+                    signal_a = 12; 
                 repeat (1) @(posedge aclk); 
-                    S_AXIS_tdata = 0;
+                    signal_a = 9;
                 repeat (1) @(posedge aclk); 
-                    S_AXIS_tdata = -5;
+                    signal_a = 6;
                 repeat (1) @(posedge aclk); 
-                    S_AXIS_tdata = -10;
+                    signal_a = 3;
                 repeat (1) @(posedge aclk); 
-                    S_AXIS_tdata = -15;
+                    signal_a = 0;
                 repeat (1) @(posedge aclk); 
-                    S_AXIS_tdata = -10;
+                    signal_a = -3;
                 repeat (1) @(posedge aclk); 
-                    S_AXIS_tdata = -5;
+                    signal_a = -6;
                 repeat (1) @(posedge aclk); 
-                    S_AXIS_tdata = 0;
+                    signal_a = -9;
                 repeat (1) @(posedge aclk); 
-                    S_AXIS_tdata = 5;
+                    signal_a = -12;
                 repeat (1) @(posedge aclk); 
-                    S_AXIS_tdata = 10;
+                    signal_a = -15;
                 repeat (1) @(posedge aclk); 
-                    S_AXIS_tdata = 15;
+                    signal_a = -12;
+                repeat (1) @(posedge aclk); 
+                    signal_a = -9;
+                repeat (1) @(posedge aclk); 
+                    signal_a = -6;
+                repeat (1) @(posedge aclk); 
+                    signal_a = -3;
+                repeat (1) @(posedge aclk); 
+                    signal_a = 0;
+                repeat (1) @(posedge aclk); 
+                    signal_a = 3;
+                repeat (1) @(posedge aclk); 
+                    signal_a = 6;
+                repeat (1) @(posedge aclk); 
+                    signal_a = 9;
+                repeat (1) @(posedge aclk); 
+                    signal_a = 12;
             end
 
+    end
+
+    initial begin
+        repeat (80) @(posedge aclk);                     
+            signal_b = -15;
     end
 
     always 
         #4 aclk = ~aclk;
 
     initial begin
-        #400
+        #1500
         $finish;
     end
 
