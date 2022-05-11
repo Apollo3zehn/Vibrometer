36d5d42f6aa277983f78832bbebe5beec6fe16c7
Apollo3zehn <git@m1.apollo3zehn.net>
2022-05-11 14:13:09 +0200

    Fix width calculation.

--- fpga/cores/axis_data_width_adapter_v1_0/axis_data_width_adapter.sv
+++ fpga/cores/axis_data_width_adapter_v1_0/axis_data_width_adapter.sv
@@ -21,7 +21,7 @@ module axis_data_width_adapter #
 );          
  
     assign M_AXIS_tvalid                    = S_AXIS_tvalid;   
-    assign M_AXIS_tdata                     = {{(AXIS_TDATA_WIDTH_OUT - AXIS_TDATA_WIDTH_IN){1'b0}}, S_AXIS_tdata};
+    assign M_AXIS_tdata                     = {{(AXIS_TDATA_WIDTH_IN - AXIS_TDATA_WIDTH_OUT){1'b0}}, S_AXIS_tdata};
     assign S_AXIS_tready                    = M_AXIS_tready;
 
 endmodule
