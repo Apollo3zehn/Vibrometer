2ee875996f5d218f965fe056dd0a776459e062cd
Apollo3zehn <git@m1.apollo3zehn.net>
2022-05-17 11:25:44 +0200

    Migrate to .NET 6.

--- src/Vibrometer.WebClient/Program.cs
+++ src/Vibrometer.WebClient/Program.cs
@@ -1,18 +1,36 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Vibrometer.WebClient.Model;
 
 namespace Vibrometer.WebClient
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Program.CreateHostBuilder(args)
-                   .Build()
-                   .Run();
-        }
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddSingleton<AppStateViewModel>();
+
+            builder.Services.AddSingleton(sp => 
+            {
+                var navigationManager = sp.GetRequiredService<NavigationManager>();
+
+                return new HubConnectionBuilder()
+                  .WithUrl(navigationManager.ToAbsoluteUri("/vibrometerhub"))
+                  .AddMessagePackProtocol()
+                  .WithAutomaticReconnect()
+                  .Build();
+            });
+
+            await builder.Build().RunAsync();
+        }
     }
 }
--- src/Vibrometer.WebServer/Program.cs
+++ src/Vibrometer.WebServer/Program.cs
@@ -1,6 +1,13 @@
-﻿using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Linq;
+using System.Net.Mime;
+using Vibrometer.API;
 
 namespace Vibrometer.WebServer
 {
@@ -8,16 +15,62 @@ namespace Vibrometer.WebServer
     {
         public static void Main(string[] args)
         {
-            Program.BuildWebHost(args).Run();
-        }
+            var builder = WebApplication.CreateBuilder(args);
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                   .UseConfiguration(new ConfigurationBuilder()
-                   .AddCommandLine(args)
-                   .Build())
-                   .UseUrls("http://0.0.0.0:8080")
-                   .UseStartup<Startup>()
-                   .Build();
+            // configuration
+            var configuration = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            builder.Configuration.AddConfiguration(configuration);
+
+            // add services
+            builder.Services.AddMvc();
+
+            builder.Services.AddSignalR(options => options.KeepAliveInterval = TimeSpan.FromSeconds(5))
+                    .AddMessagePackProtocol();
+
+            builder.Services.AddResponseCompression(options =>
+            {
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[]
+                {
+                    MediaTypeNames.Application.Octet,
+                    "application/wasm"
+                });
+            });
+
+            builder.Services.AddSingleton<AppState>();
+            builder.Services.AddSingleton<VibrometerApi>();
+            builder.Services.AddSingleton<ClientPushService>();
+
+            // build
+            var app = builder.Build();
+
+            // configure pipeline
+            app.UseResponseCompression();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
+            }
+
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<VibrometerHub>("/vibrometerhub", options =>
+                {
+                    options.ApplicationMaxBufferSize = 3 * 1024 * 1024; // to send the bitstream (see this issue: https://github.com/aspnet/SignalR/issues/2266#issuecomment-389143453)
+                });
+                endpoints.MapFallbackToFile("index.html");
+            });
+
+            // Run
+            var baseUrl = "https://0.0.0.0:8443";
+            app.Run(baseUrl);
+        }
     }
 }
